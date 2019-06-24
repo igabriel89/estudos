9dbdf59506f4c651115dfcc17fe7bbfac43a15c5
36711146816 <36711146816@RR-298430.ibama.gov.br>
2019-06-24 15:26:54 -0400

    Criação da Tela - Cadastro de Pedidos

---
+++ PedidoVendaEstudos/src/main/java/br/com/igabriel89/pedidovenda/controller/CadastroPedidoBean.java
@@ -0,0 +1,23 @@
+package br.com.igabriel89.pedidovenda.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class CadastroPedidoBean {
+	private List<Integer> itens;
+
+	public CadastroPedidoBean() {
+		this.itens = new ArrayList<Integer>();
+		this.itens.add(1);
+	}
+
+	public List<Integer> getItens() {
+		return itens;
+	}
+
+}
